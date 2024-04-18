3f235d9e6c69070559d0510441de324b4b0c9d75
Gabriel Poma <gpoma@24eme.fr>
2024-04-18 18:30:03 +0200

    nav centrée

--- app/views/layout.html.php
+++ app/views/layout.html.php
@@ -11,8 +11,8 @@
     <link href="<?php echo $urlbase; ?>/css/nutrivin.css" rel="stylesheet" />
   </head>
   <body>
-    <nav class="navbar start-50">
-      <img src="/images/logo.svg" alt="Bootstrap" width="64" height="64">
+    <nav class="navbar">
+      <img src="/images/logo.svg" class="mx-auto d-block" alt="Logo nutrivin" width="64" height="64">
     </nav>
 
     <div class="container-sm p-3">
