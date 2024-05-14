eacaeddebafd8cbadebb6ee03c704be066bcba3a
Gabriel Poma <gpoma@24eme.fr>
2024-05-14 17:41:47 +0200

    row pas de qrcode fait la taille du tableau

--- app/views/qrcode_list.html.php
+++ app/views/qrcode_list.html.php
@@ -21,7 +21,7 @@
     </thead>
     <?php if (!$qrlist): ?>
         <tbody>
-            <tr><td colspan=3><center><i>Vous n'avez pas encore créé de QRCode</i></center></td></tr>
+            <tr><td colspan=4><center><i>Vous n'avez pas encore créé de QRCode</i></center></td></tr>
         </tbody>
     <?php else: ?>
         <tbody>
