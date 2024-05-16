97d9568a08d537227c7ede6ad0db29ea4ba28cbf
Tangui Morlier <tangui@tangui.eu.org>
2024-05-16 17:14:47 +0200

    Meilleure détection de la valeur par défaut pour viticonnect

--- config/config.php
+++ config/config.php
@@ -53,6 +53,5 @@ $config = [
     'qrcode' => [
         'color' => "#000000",
         'logo'  => __DIR__.'/../web/images/ivso.svg',
-    ],
-    'viticonnect_baseurl' => 'https://viticonnect.net/cas'
+    ]
 ];
