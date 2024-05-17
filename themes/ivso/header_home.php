cd391b2ca117a14d118c9b084e248baa1f8e082b
Vincent LAURENT <vince.laurent@gmail.com>
2024-05-17 18:46:10 +0200

    Logo un peu plus petit

--- themes/ivso/header_home.php
+++ themes/ivso/header_home.php
@@ -1,3 +1,3 @@
 <?php include($THEME.'css.php'); ?>
 
-<img class="mb-3" src="https://www.vignobles-sudouest.fr/wp-content/uploads/2019/12/IVSO-Logo-V.png" height="275px" />
+<img class="mb-3" src="https://www.vignobles-sudouest.fr/wp-content/uploads/2019/12/IVSO-Logo-V.png" height="260px" />
