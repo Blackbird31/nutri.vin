873eaed9e0f2ccea542d5207b6a473fbbcf3c0aa
Gabriel Poma <gpoma@24eme.fr>
2024-05-30 11:48:14 +0200

    theme en variable d'env

--- config/config.php
+++ config/config.php
@@ -54,6 +54,7 @@ $config = [
         'color' => "#000000",
         'logo'  => __DIR__.'/../web/images/ivso.svg',
     ],
+    'theme' => getenv('THEME') ?: 'nutrivin',
 //    'db_pdo' => 'couchdb:http://admin:admin@127.0.0.1:5984/nutrivin_test',
 //    'admin_user' => 'userid',
 ];
