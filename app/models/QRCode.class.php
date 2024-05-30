0dd796c8f1916d5dd71a052031e819b9c57f1818
JB LM <lemetayer.jb@gmail.com>
2024-05-30 21:44:47 +0200

    on deplace la fct de resize d'image dans la class QRCode

--- app/models/QRCode.class.php
+++ app/models/QRCode.class.php
@@ -506,4 +506,40 @@ class QRCode extends Mapper
         }
         return substr($this->responsable_siret, 0, 9);
     }
+
+    private function resizeImage($image, $max) {
+      if (!is_file($image)) {
+        return false;
+      }
+      $size = getimagesize($image);
+      $width = $size[0];
+      $height = $size[1];
+      $mime = $size['mime'];
+      if ($width <= $max && $height <= $max) {
+        return $image;
+      }
+      $ratio = $width / $height;
+      if ($width > $height) {
+        $newWidth = $max;
+        $newHeight = $max / $ratio;
+      } else {
+        $newHeight = $max;
+        $newWidth = $max * $ratio;
+      }
+      $newImage = imagecreatetruecolor($newWidth, $newHeight);
+      if ($mime == 'image/jpeg') {
+        $source = imagecreatefromjpeg($image);
+        imagecopyresampled($newImage, $source, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+        imagejpeg($newImage, $image);
+      } elseif ($mime == 'image/png') {
+        $source = imagecreatefrompng($image);
+        imagecopyresampled($newImage, $source, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+        imagepng($newImage, $image);
+      } else {
+        return false;
+      }
+      imagedestroy($source);
+      imagedestroy($newImage);
+      return $image;
+    }
 }
